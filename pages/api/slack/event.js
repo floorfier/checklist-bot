ba31437f65c5c46c1f9eaaf46bb8d8fa527adff6
Jose Vera <jose.vera@floorfy.com>
2025-07-14 11:22:05 +0200

    Event.js

--- pages/api/slack/event.js
+++ pages/api/slack/event.js
@@ -4,21 +4,17 @@ export const config = {
   },
 };
 
-export default function handler(req, res) {
-  if (req.method !== 'POST') {
-    res.setHeader('Allow', 'POST');
-    return res.status(405).send('Method Not Allowed');
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const { type, challenge } = req.body;
+
+    if (type === 'url_verification') {
+      return res.status(200).send(challenge);
+    }
+
+    return res.status(200).end();
   }
 
-  const { type, challenge } = req.body;
-
-  console.log('Slack event:', req.body); // para debug en Vercel
-
-  if (type === 'url_verification') {
-    // Responder exactamente con el challenge, texto plano
-    return res.status(200).send(challenge);
-  }
-
-  // Otros eventos
-  return res.status(200).end();
+  res.setHeader('Allow', 'POST');
+  res.status(405).end('Method Not Allowed');
 }
